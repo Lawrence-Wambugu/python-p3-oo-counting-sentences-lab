a191840a4bdd8b20fdc640b6391878fc367138fa
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-05-11 17:55:42 +0300

    pass lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value  # Triggers setter validation
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        # Split on punctuation that typically ends sentences
+        sentences = re.split(r'[.!?]+', self._value)
+        # Filter out empty strings and whitespace-only strings
+        return len([s for s in sentences if s.strip()])
